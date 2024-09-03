8b2148be27c06bfb3959a471baf734f2f2453d24
Correct Username <correct@example.com>
2024-09-03 09:59:07 +0530

    added

--- index.js
+++ index.js
@@ -265,7 +265,7 @@ io.on('connection', (socket) => {
       const key = findKeyByValue(userSocketMap, socket.id);
       console.log(key)
       if (key) {
-        console.log(`Stopping search for user: ${key}`);
+        console.log(`Stopping search forr user: ${key}`);
         userSocketMap.delete(key);
         socket.emit('stoppedSearch');
       } else {
