0b4e7df30e09c8e20488bb23bc536c424ce9e1a9
Correct Username <correct@example.com>
2024-09-13 08:05:01 +0530

    hjewdfscx

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = new Server(server, {
   }
 });
 
-var userSocketMap = new Map();
+let userSocketMap = new Map();
 
 app.use(express.json());
 app.set('views', __dirname + '/public/views');
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 
 // API Endpoints
 app.post('/api/deviceid', async (req, res) => {
-  try {
-    const { deviceid } = req.body;
-    if (!deviceid) {
-      return res.status(400).json({ error: 'Device ID is required' });
-    }
+  const { deviceid } = req.body;
+  if (!deviceid) {
+    return res.status(400).json({ error: 'Device ID is required' });
+  }
 
+  try {
     const existingDevice = await prisma.deviceid.findUnique({ where: { deviceid } });
     if (existingDevice) {
       return res.status(200).json({ status: 'false', message: 'Device ID already exists', device: existingDevice });
@@ -39,7 +39,6 @@ app.post('/api/deviceid', async (req, res) => {
 
     const device = await prisma.deviceid.create({ data: { deviceid, available: false } });
     res.status(201).json({ status: 'true', device });
-
   } catch (error) {
     console.error('Error creating or retrieving device ID:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -47,12 +46,12 @@ app.post('/api/deviceid', async (req, res) => {
 });
 
 app.post('/api/appclose', async (req, res) => {
-  try {
-    const { deviceid } = req.body;
-    if (!deviceid) {
-      return res.status(400).json({ error: 'Device ID is required' });
-    }
+  const { deviceid } = req.body;
+  if (!deviceid) {
+    return res.status(400).json({ error: 'Device ID is required' });
+  }
 
+  try {
     const device = await prisma.deviceid.findUnique({ where: { deviceid } });
     if (!device) {
       return res.status(404).json({ error: 'Device not found' });
@@ -64,7 +63,6 @@ app.post('/api/appclose', async (req, res) => {
     });
 
     res.status(200).json({ status: 'true', device: updatedDevice });
-
   } catch (error) {
     console.error('Error closing app for device ID:', error);
     res.status(500).json({ error: 'Internal server error' });
@@ -72,12 +70,12 @@ app.post('/api/appclose', async (req, res) => {
 });
 
 app.post('/api/deviceid/report', async (req, res) => {
-  try {
-    const { deviceid } = req.body;
-    if (!deviceid) {
-      return res.status(400).json({ error: 'Device ID is required' });
-    }
+  const { deviceid } = req.body;
+  if (!deviceid) {
+    return res.status(400).json({ error: 'Device ID is required' });
+  }
 
+  try {
     const device = await prisma.deviceid.findUnique({ where: { deviceid } });
     if (!device) {
       return res.status(404).json({ error: 'Device not found' });
@@ -145,32 +143,26 @@ io.on('connection', (socket) => {
         if (receiverId !== senderId) {
           const room = [senderId, receiverId].sort().join('-');
 
-          // Check if socket is in any room
           if (socket.rooms.size === 1) {
-            // Socket is not in any room, so join the room
             socket.join(room);
             io.to(socket.id).emit('joined', room);
 
             const receiverSocketId = userSocketMap.get(receiverId);
-            const isDeleted = userSocketMap.delete(receiverId);
-
-            if (isDeleted) {
+            if (receiverSocketId) {
+              userSocketMap.delete(receiverId);
               await prisma.deviceid.updateMany({
-                where: { id: { in: [parseInt(senderId), parseInt(receiverId)] } },
+                where: { id: { in: [senderId, receiverId] } },
                 data: { available: false },
               });
               io.to(receiverSocketId).emit('roomReady', room);
             }
           } else {
-            console.log(`Socket ${socket.id} is already in a room`);
             io.to(socket.id).emit('alreadyInRoom');
           }
         }
       } else {
-        // Only add to map if not already in a room
         if (socket.rooms.size === 1) {
           userSocketMap.set(senderId, socket.id);
-          console.log(userSocketMap);
           io.to(socket.id).emit('searching');
         }
       }
@@ -181,13 +173,10 @@ io.on('connection', (socket) => {
 
   socket.on('joinRoom', (room) => {
     try {
-      // Check if socket is in any room
       if (socket.rooms.size === 1) {
-        // Socket is not in any room, so join the room
         socket.join(room);
         io.to(socket.id).emit('joined', room);
       } else {
-        console.log(`Socket ${socket.id} is already in a room`);
         io.to(socket.id).emit('alreadyInRoom');
       }
     } catch (err) {
@@ -197,9 +186,7 @@ io.on('connection', (socket) => {
 
   socket.on('message', async ({ message, deviceId, room }) => {
     try {
-      console.log(`Received message for room ${room}:`, { message, deviceId });
       io.to(room).emit('message', { message, deviceId });
-      console.log(`Message sent to room ${room}: ${message}`);
     } catch (error) {
       console.error('Error sending message:', error);
     }
@@ -207,28 +194,19 @@ io.on('connection', (socket) => {
 
   socket.on('skip', async (room) => {
     try {
-      // Emit a clearChat message to the room
       io.to(room).emit('clearChat');
-
-      // Optional: Wait a short time to ensure clients process the clearChat message
       await new Promise(resolve => setTimeout(resolve, 100));
 
-      // Fetch all sockets in the room
       const socketsInRoom = await io.in(room).fetchSockets();
       const socketIds = socketsInRoom.map(socketInRoom => socketInRoom.id);
 
-      // Make each socket leave the room
       socketsInRoom.forEach(socketInRoom => {
         socketInRoom.leave(room);
-        console.log(`Socket with ID ${socketInRoom.id} has left the room: ${room}`);
       });
 
-      // Emit reJoin event to each of the sockets that left the room
       socketIds.forEach(socketId => {
         io.to(socketId).emit('reJoin', room);
-        console.log(`Socket with ID ${socketId} has been skipped and trying to rejoin with another room`);
       });
-
     } catch (error) {
       console.error('Error handling skip:', error);
     }
@@ -260,20 +238,25 @@ io.on('connection', (socket) => {
       if (connectedUsers.length > 0) {
         const randomIndex = Math.floor(Math.random() * connectedUsers.length);
         const receiverId = connectedUsers[randomIndex];
-        if (receiverId !== senderId) {
+
+        if (senderId !== receiverId) {
           const newRoom = [senderId, receiverId].sort().join('-');
-          socket.join(newRoom);
-          io.to(socket.id).emit('joined', newRoom);
 
-          const receiverSocketId = userSocketMap.get(receiverId);
-          const isDeleted = userSocketMap.delete(receiverId);
+          if (socket.rooms.size === 1) {
+            socket.join(newRoom);
+            io.to(socket.id).emit('joined', newRoom);
 
-          if (isDeleted) {
-            await prisma.deviceid.updateMany({
-              where: { id: { in: [parseInt(senderId), parseInt(receiverId)] } },
-              data: { available: false },
-            });
-            io.to(receiverSocketId).emit('roomReady', newRoom);
+            const receiverSocketId = userSocketMap.get(receiverId);
+            if (receiverSocketId) {
+              userSocketMap.delete(receiverId);
+              await prisma.deviceid.updateMany({
+                where: { id: { in: [senderId, receiverId] } },
+                data: { available: false },
+              });
+              io.to(receiverSocketId).emit('roomReady', newRoom);
+            }
+          } else {
+            io.to(socket.id).emit('alreadyInRoom');
           }
         }
       } else {
@@ -287,26 +270,49 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('disconnect', async () => {
-    console.log(`Socket ${socket.id} disconnected`);
-    userSocketMap.forEach(async (value, key) => {
-      if (value === socket.id) {
-        userSocketMap.delete(key);
+  socket.on('stopSearch', async (deviceId) => {
+    try {
+      const device = await prisma.deviceid.findUnique({ where: { deviceid: deviceId } });
+      const senderId = device.id;
 
-        // Update device status to available
+      if (device) {
+        userSocketMap.delete(senderId);
         await prisma.deviceid.update({
-          where: { id: key },
-          data: { available: true }
+          where: { deviceid: deviceId },
+          data: { available: false },
         });
-
-        // Emit reJoin event to all users to pair with a new user
-        io.emit('reJoin', key);
-        console.log(`Socket ${socket.id} has been removed from userSocketMap`);
       }
-    });
+    } catch (error) {
+      console.error('Error stopping search:', error);
+    }
   });
+
+  socket.on('disconnectRoom', async (room) => {
+    try {
+      const socketsInRoom = await io.in(room).fetchSockets();
+      const remainingSockets = socketsInRoom.length;
+
+      if (remainingSockets <= 1) {
+        const remainingSocketId = remainingSockets ? socketsInRoom[0].id : null;
+        if (remainingSocketId) {
+          io.to(remainingSocketId).emit('pairedWithDevice', null);
+          await prisma.deviceid.updateMany({
+            where: { id: { in: [socket.id] } },
+            data: { available: true },
+          });
+        }
+      }
+    } catch (error) {
+      console.error('Error handling disconnect room:', error);
+    }
+  });
+
+  socket.on('error', (error) => {
+    console.error('Socket.IO Error:', error);
+  });
+
 });
 
 server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is listening on port 3000');
 });
